73d4961b15e784200e707202f5fba1124d2da24a
nexusthede <ivanrrr76@gmail.com>
2026-07-12 21:50:50 -0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    prefix: ".",
+
+    embedColor: "#2b2d31",
+
+    token: process.env.TOKEN,
+
+    mongoURI: process.env.MONGO_URI,
+
+    leaderboardUpdateInterval: 60000,
+
+    weeklyResetDays: 7,
+
+    leaderboardLimit: 10
+
+};
