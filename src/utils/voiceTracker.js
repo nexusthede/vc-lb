fda0bf9fda8b275839680278f796eb989f36b3de
nexusthede <ivanrrr76@gmail.com>
2026-07-12 21:54:36 -0700

    Create voiceTracker.js

---
+++ src/utils/voiceTracker.js
@@ -0,0 +1,98 @@
+const User = require("../models/User");
+
+
+const activeVoiceUsers = new Map();
+
+
+
+function addUser(member){
+
+    activeVoiceUsers.set(
+
+        `${member.guild.id}-${member.id}`,
+
+        {
+            guildId: member.guild.id,
+            userId: member.id
+        }
+
+    );
+
+}
+
+
+
+function removeUser(member){
+
+    activeVoiceUsers.delete(
+
+        `${member.guild.id}-${member.id}`
+
+    );
+
+}
+
+
+
+async function updateVoiceTime(){
+
+
+    const updates = [];
+
+
+    for(const user of activeVoiceUsers.values()){
+
+
+        updates.push({
+
+            updateOne:{
+
+                filter:{
+
+                    guildId:user.guildId,
+                    userId:user.userId
+
+                },
+
+
+                update:{
+
+                    $inc:{
+                        voiceTime:60
+                    }
+
+                },
+
+
+                upsert:true
+
+            }
+
+        });
+
+
+    }
+
+
+
+    if(updates.length){
+
+        await User.bulkWrite(
+            updates
+        );
+
+    }
+
+
+}
+
+
+
+module.exports = {
+
+    activeVoiceUsers,
+    addUser,
+    removeUser,
+    updateVoiceTime
+
+};
