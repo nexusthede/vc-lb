c4bbebba90bcf5df9664901195951456f7315b06
nexusthede <ivanrrr76@gmail.com>
2026-07-12 21:55:17 -0700

    Create voiceStateUpdate.js

---
+++ src/events/voiceStateUpdate.js
@@ -0,0 +1,65 @@
+const {
+    addUser,
+    removeUser
+} = require("../utils/voiceTracker");
+
+
+
+module.exports = {
+
+
+    name:"voiceStateUpdate",
+
+
+
+    async execute(
+        oldState,
+        newState
+    ){
+
+
+        const member =
+        newState.member ||
+        oldState.member;
+
+
+
+        if(
+            !member ||
+            member.user.bot
+        )
+            return;
+
+
+
+
+        // Join VC
+
+        if(
+            !oldState.channel &&
+            newState.channel
+        ){
+
+            addUser(member);
+
+        }
+
+
+
+        // Leave VC
+
+        if(
+            oldState.channel &&
+            !newState.channel
+        ){
+
+            removeUser(member);
+
+        }
+
+
+
+    }
+
+
+};
