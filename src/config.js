dfd1692d194600b6ed04ddc565dfe62c585f5575
nexusthede <ivanrrr76@gmail.com>
2026-07-14 01:18:29 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,12 @@ module.exports = {
 
     embedColor: "#2b2d31",
 
+    successColor: "#57F287",
+
+    errorColor: "#ED4245",
+
+    warningColor: "#FEE75C",
+
     token: process.env.TOKEN,
 
     mongoURI: process.env.MONGO_URI,
@@ -12,6 +18,21 @@ module.exports = {
 
     weeklyResetDays: 7,
 
-    leaderboardLimit: 10
+    leaderboardLimit: 10,
+
+
+    voiceChannels: {
+
+        joinToCreate: "1524179518229839912",
+
+        random: "1526402275218686054",
+
+        public: "1524178779939803268",
+
+        private: "1524178808692019423",
+
+        unmute: "1524179582662738071"
+
+    }
 
 };
