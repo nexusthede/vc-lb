d475cf77709a153e807c593dd0da484ac8859855
nexusthede <ivanrrr76@gmail.com>
2026-07-14 01:34:24 -0700

    Create unlock.js

---
+++ src/commands/vc/unlock.js
@@ -0,0 +1,110 @@
+const { EmbedBuilder } = require("discord.js");
+const config = require("../../config");
+const VoiceChannel = require("../../models/VoiceChannel");
+
+
+module.exports = {
+
+    name: "unlock",
+
+    async execute(client, message, args) {
+
+
+        const channel =
+        message.member.voice.channel;
+
+
+        if(!channel){
+
+            return message.channel.send({
+
+                embeds:[
+
+                    new EmbedBuilder()
+
+                    .setColor(config.errorColor)
+
+                    .setDescription(
+                        `<:x_failed:1526498753798738010> You must be connected to a voice channel to use this command.`
+                    )
+
+                ]
+
+            });
+
+        }
+
+
+
+        const data =
+        await VoiceChannel.findOne({
+
+            channelId: channel.id
+
+        });
+
+
+
+        if(!data || data.ownerId !== message.author.id){
+
+            return message.channel.send({
+
+                embeds:[
+
+                    new EmbedBuilder()
+
+                    .setColor(config.errorColor)
+
+                    .setDescription(
+                        `<:x_failed:1526498753798738010> You don't own **${channel.name}**.`
+                    )
+
+                ]
+
+            });
+
+        }
+
+
+
+        await channel.permissionOverwrites.edit(
+
+            message.guild.roles.everyone,
+
+            {
+                Connect:null
+            }
+
+        );
+
+
+
+        if(config.voiceCategories?.public){
+
+            await channel.setParent(
+                config.voiceCategories.public
+            );
+
+        }
+
+
+
+        const embed = new EmbedBuilder()
+
+        .setColor(config.successColor)
+
+        .setDescription(
+            `<:vc_unlock:1526495168130318348> **${channel.name}** has been unlocked.`
+        );
+
+
+        message.channel.send({
+
+            embeds:[embed]
+
+        });
+
+
+    }
+
+};
