4ae02bc258c90fd91dc54a1c8049a1d01d27afb3
nexusthede <ivanrrr76@gmail.com>
2026-07-14 01:43:36 -0700

    Update and rename VoiceMaster.js to voiceMaster.js

---
+++ src/events/voiceMaster.js
@@ -0,0 +1,137 @@
+const config = require("../config");
+
+const {
+    createVoice,
+    deleteVoice
+} = require("../utils/voiceHandler");
+
+const {
+    savePreviousChannel,
+    getPreviousChannel
+} = require("../utils/unmuteTracker");
+
+
+module.exports = {
+
+    name:"voiceStateUpdate",
+
+
+    async execute(oldState,newState){
+
+
+        const member =
+        newState.member ||
+        oldState.member;
+
+
+        if(!member || member.user.bot)
+            return;
+
+
+
+        // Save last VC
+
+        if(oldState.channelId){
+
+            savePreviousChannel(
+                member,
+                oldState.channel
+            );
+
+        }
+
+
+
+        // Join To Create
+
+        if(
+            newState.channelId ===
+            config.voiceChannels.joinToCreate
+        ){
+
+            await createVoice(member);
+
+        }
+
+
+
+        // Unmute VC
+
+        if(
+            newState.channelId ===
+            config.voiceChannels.unmute
+        ){
+
+
+            // Normal users stay
+
+            if(
+                !member.voice.serverMute &&
+                !member.voice.serverDeaf
+            )
+                return;
+
+
+
+            const previous =
+            getPreviousChannel(
+                member.id
+            );
+
+
+            if(!previous)
+                return;
+
+
+
+            const channel =
+            member.guild.channels.cache.get(
+                previous
+            );
+
+
+            if(channel){
+
+                await member.voice.setChannel(
+                    channel
+                );
+
+            }
+
+
+        }
+
+
+
+        // Delete empty VoiceMaster rooms
+
+        if(oldState.channel){
+
+            const channel =
+            oldState.channel;
+
+
+            if(
+
+                channel.parentId ===
+                config.voiceCategories.public ||
+
+                channel.parentId ===
+                config.voiceCategories.private
+
+            ){
+
+                if(channel.members.size === 0){
+
+                    await deleteVoice(channel);
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+};
