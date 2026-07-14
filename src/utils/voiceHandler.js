65f4ec056e32c6c53871ec1e564f811fc2d731a8
nexusthede <ivanrrr76@gmail.com>
2026-07-14 01:38:39 -0700

    Create voiceHandler.js

---
+++ src/utils/voiceHandler.js
@@ -0,0 +1,73 @@
+const {
+    ChannelType
+} = require("discord.js");
+
+const config = require("../config");
+
+const VoiceChannel = require("../models/VoiceChannel");
+
+
+async function createVoice(member){
+
+
+    const channel =
+    await member.guild.channels.create({
+
+        name:`${member.displayName}'s Room`,
+
+        type:ChannelType.GuildVoice,
+
+        parent:
+        config.voiceCategories.public
+
+    });
+
+
+
+    await VoiceChannel.create({
+
+        guildId:member.guild.id,
+
+        channelId:channel.id,
+
+        ownerId:member.id
+
+    });
+
+
+
+    await member.voice.setChannel(channel);
+
+
+
+    return channel;
+
+}
+
+
+
+async function deleteVoice(channel){
+
+
+    await VoiceChannel.deleteOne({
+
+        channelId:channel.id
+
+    });
+
+
+    await channel.delete()
+    .catch(()=>{});
+
+
+}
+
+
+
+module.exports = {
+
+    createVoice,
+
+    deleteVoice
+
+};
